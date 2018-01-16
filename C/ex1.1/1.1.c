a6a2e85d9fdb27abe6d7bc4135d3c404d9a53b70
Caio R BCarmo <bcarmo@geru.com.br>
2018-01-16 02:16:06 -0200

    improved ex1.1

--- C/ex1.1/1.1.c
+++ C/ex1.1/1.1.c
@@ -1,34 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * Your code only works for one single value. The requirement asks for a
- * collection of positive integers ending with 0.
- *
- * */
 int main()
 {
-	int number, secnumber, thirnumber;
-	number= 0; /* insert a space after '=' sign */
-	secnumber= 0;
-	thirnumber= 0;
+	int number;
+	scanf("%d", &number);
 
-	printf("Insert a collection of positive numbers: ");
-	scanf("%d %d %d", &number, &secnumber, &thirnumber);
+	while (number > 0) {
+		printf("%d ", number * number);
+		scanf("%d", &number);
+	}
 
-	if(number < 0 || secnumber < 0 || thirnumber < 0)
-	{
-		printf("Insert a number > 0. Please execute it again.\n");
-		return 0;
-	}
-	else
-	{
-		number= number * number;
-		secnumber= secnumber * secnumber;
-		thirnumber= thirnumber * thirnumber;
-		printf("They square are: %d, %d, %d\n", number, secnumber,
-				thirnumber);	
-	}
-	
+	printf("\n");
 	return 0;
 }
 
