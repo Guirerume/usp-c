4ed62260be17ee4797a1321e1c07a5faa329e4fb
Guirerume <gpaula@2mundos.net>
2018-02-17 22:19:31 -0200

    Create 1.2.c

--- C/ex1.2/1.2.c
+++ C/ex1.2/1.2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int n;
+	int sum = 0;
+
+    scanf("%d", &n);
+    printf("\n");
+
+	for(int count = 0; n >= count; count++) {
+        sum += count;
+	}
+
+	printf("%d", sum);
+	return 0;
+}
 
